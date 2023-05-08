72a4b9be4bf3920a68ca905d2ab68d81dcde3faa
Pium Sudhara <pium.karunasena@gmail.com>
2023-05-08 22:39:26 +0530

    Edit Index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Application Listening on port 3000");
-});
+app.use(cors());
+app.get("/", (req, res) => res.json("Hello From Amazon EKS"));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
